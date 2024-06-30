a251f3d83feadd0946946a3ceba171bed1c78438
Isha Savani <isha.savani@gmail.com>
2024-06-30 12:51:49 +0200

    updated page

--- src/components/pages/civicEngagement.tsx
+++ src/components/pages/civicEngagement.tsx
@@ -14,6 +14,103 @@ export default function CivicEngagementPage() {
         informing my research methodology and bridging the gap between
         theoretical knowledge and the reality of the ground. Selected avenues of
         engagement are mentioned below:
+        <h4>Hosting President of Somalia to Oslo Science Park </h4>
+        <br />
+        <br />
+        <span className="italic font-bold">
+          We had the opportunity to host the President of Somalia
+          (2022-present), His Excellency Hassan Sheikh Mohamud and his advisors,
+          along with Norwegian Ambassador to Kenya Mr. Gunnar Andreas Holm, for
+          an exiting discussion on the indications of fresh water in deep
+          aquifers across Somalia, and on how to incorporate hydroclimatic
+          extremes- droughts and floods, into sustainability quantification
+          based on averaging over historic climate data.
+        </span>
+        <br />
+        Oslo Science Park, June 2024
+        <br />
+        <br />
+        <span className="italic font-bold">
+          Introduction to Somalia deep groundwater project and geo-thermal
+          energy systems in the context of prevailing water scarcity and energy
+          demands in India
+        </span>
+        <br />
+        <br />
+        <span className="italic font-bold">
+          Introduction to Somalia deep groundwater project and geo-thermal
+          energy systems in the context of prevailing water scarcity and energy
+          demands in India
+        </span>
+        <br />
+        This talk was given at two different occasion in 2023 to the Indian
+        Ambassador to Norway (B. Bala Bhaskar), and to representatives from the
+        Indian Embassy and tech firm Tech Inde Ltd, Oslo, August 2022, February
+        2023
+        <br />
+        <br />
+        <span className="italic font-bold">
+          The role of sustainable and reliable sources of water in addressing
+          food security
+        </span>
+        <br />
+        To the Government of Norway, Ministry of Foreign Affairs, Department of
+        water, climate change and sustainability, Oslo, Noway, February 2023
+        <br />
+        <br />
+        <span className="italic font-bold">
+          The multidisciplinary field of deep groundwater and indications of
+          water in Somali Peninsula
+        </span>
+        <br />
+        To the Government of Puntland, Minister of Water and Minerals, March
+        2022 to Oslo (Talk given at Ruden AS's headquarters)
+        <br />
+        <br />
+        <span className="italic font-bold">
+          How the Somali diaspora can help with water scarcity challenges
+        </span>
+        <br />
+        To the Government of Puntland, Advisors to the Department of Diaspora
+        Affairs, April 2022 (Virtual talk)
+        <br />
+        <br />
+        <span className="italic font-bold">
+          There can be no food security without sustainable and reliable source
+          of water
+        </span>
+        <br />
+        During the occasion of the Norwegian crown prince’s visit to Nairobi,
+        Kenya, November 24-26, 2022. Side note: I unfortunately could not attend
+        the event due to catching covid. My colleague delivered the talk
+        instead.
+        <br />
+        <br />
+        <span className="italic font-bold">
+          Repurposing oil and gas data in search of new fresh groundwater
+        </span>
+        <br />
+        To the National Irrigation Authorities, Government of Kenya, Nairobi,
+        May 2022
+        <br />
+        <br />
+        <span className="italic font-bold">
+          Somalia deep ground water project and implications for the future of
+          water security
+        </span>
+        <br />
+        At the occation of the Round table conference bringing together
+        Norwegian Ambassador Gunnar Andreas Holm, World Food Program, Innovation
+        Norway at the Agri-Delegation Conference, Nairobi, Kenya, April 2022
+        <br />
+        <br />
+        <span className="italic font-bold">
+          Computing groundwater recharge in the Horn of Africa in the absence of
+          ground measurements: opportunities and challenges
+        </span>{' '}
+        <br />
+        To the United Nations Food and Agriculture Organization, Nairobi, Kenya,
+        April 2022
         <br />
         <br />
         <h4>Seminars and educational programs (knowledge sharing)</h4>
@@ -35,13 +132,13 @@ export default function CivicEngagementPage() {
         Pharo Foundation, and to be implemented in East Africa. 2023-present
         <br />
         <br />
-        <h4>Dialogues (mutual understanding)</h4>
+        <h4>Dialogues</h4>
         <br />
         <span className="italic font-bold">
           Dialogues on the meaning of sustainability with regards to water
         </span>
         <br />
-        Fairtrade International, SLB (oil and gas industry), Mestergrønn
+        With Fairtrade International, SLB (oil and gas industry), Mestergrønn
         (leading flower industry of Norway), April 2023 – present
         <br />
         <br />
@@ -51,84 +148,11 @@ export default function CivicEngagementPage() {
           sector to solve global challenges{' '}
         </span>
         <br />
-        International Federation of Red Cross
+        With International Federation of Red Cross
         <br />
         Oslo, February 2022 (Dialogue took place at at Ruden headquarters)
         <br />
         <br />
-        <h4>Talks to Governmental bodies (Science-policy interface) </h4>
-        <br />
-        <span className="italic font-bold">
-          Introduction to Somalia deep groundwater project and geo-thermal
-          energy systems in the context of prevailing water scarcity and energy
-          demands in India
-        </span>
-        <br />
-        This talk was given at two different occasion in 2023 to the Indian
-        Ambassador to Norway (B. Bala Bhaskar), and to representatives from the
-        Indian Embassy and tech firm Tech Inde Ltd, Oslo, August 2022, February
-        2023
-        <br />
-        <br />
-        <span className="italic font-bold">
-          The role of sustainable and reliable sources of water in addressing
-          food security
-        </span>
-        <br />
-        Government of Norway, Ministry of Foreign Affairs, Department of water,
-        climate change and sustainability, Oslo, Noway, February 2023
-        <br />
-        <br />
-        <span className="italic font-bold">
-          The multidisciplinary field of deep groundwater and indications of
-          water in Somali Peninsula
-        </span>
-        <br />
-        Government of Puntland, Minister of Water and Minerals, March 2022 to
-        Oslo (Talk given at Ruden AS's headquarters)
-        <br />
-        <br />
-        <span className="italic font-bold">
-          How the Somali diaspora can help with water scarcity challenges
-        </span>
-        <br />
-        Government of Puntland, Advisors to the Department of Diaspora Affairs,
-        April 2022 (Virtual talk)
-        <br />
-        <br />
-        <span className="italic font-bold">
-          There can be no food security without sustainable and reliable source
-          of water
-        </span>
-        <br />
-        Norway, Sweden, and Kenya Pioneering Sustainability - together (Official
-        crown prince’s visit), Nairobi, Kenya, November 24-26, 2022
-        <br />
-        <br />
-        <span className="italic font-bold">
-          Repurposing oil and gas data in search of new fresh groundwater
-        </span>
-        <br />
-        National Irrigation Authorities, Government of Kenya, Nairobi, May 2022
-        <br />
-        <br />
-        <span className="italic font-bold">
-          Somalia deep ground water project and implications for the future of
-          water security
-        </span>
-        <br />
-        Round table conference bringing together Norwegian Ambassador, World
-        Food Program, Innovation Norway at the Agri-Delegation Conference,
-        Nairobi, Kenya, April 2022
-        <br />
-        <br />
-        <span className="italic font-bold">
-          Computing groundwater recharge in the Horn of Africa in the absence of
-          ground measurements: opportunities and challenges
-        </span>{' '}
-        <br />
-        United Nations Food and Agriculture Organization, Nairobi, Kenya, April
-        2022
       </BasePage>
     </div>
   )
